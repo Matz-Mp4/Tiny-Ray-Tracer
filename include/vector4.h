5f8483bc790983c5ac59e5e4f7ea8bee48b4635a
Arthur Bartol <arthur.b4rtol@gmail.com>
2024-04-13 18:36:46 -0300

    Fixed formatting

--- include/vector4.h
+++ include/vector4.h
@@ -13,26 +13,29 @@ public:
     double w;
 
     Vector4() = default;
+    ~Vector4() = default;
+
     Vector4(double t);
     Vector4(double x, double y, double z);
     Vector4(double x, double y, double z, double w);
     
-    double          operator*(const Vector4&) const; //dot product
-    Vector4         operator+(const Vector4& ) const;
-    Vector4  operator*(double) const; 
-    Vector4  operator/(double ) const;
- 
-    Vector4         operator-(const Vector4& ) const;
-    Vector4         operator-() const;
-    bool            operator==(const Vector4& ) const;
+    Vector4 operator+(const Vector4& rhs) const;
+    Vector4 operator-(const Vector4& rhs) const;
+    Vector4 operator-(                  ) const;
+    double  operator*(const Vector4& rhs) const; //dot product
+    Vector4 operator*(double         rhs) const;
+    Vector4 operator/(double         rhs) const;
+
+    bool    operator==(const Vector4& rhs) const;
+
     friend std::ostream& operator<<(std::ostream& stream, const Vector4& vec); //print Vector4
-                                                                               //
-    double length() const;    
+
+    double length() const;
 };
 
-Vector4 cross(const Vector4& u, const Vector4& v);
-Vector4  operator*(double, const Vector4&) ; 
-Vector4  operator/(double, const Vector4&);
- 
-
 using Point4 = Vector4;
+
+Vector4 operator*(double, const Vector4& rhs);
+Vector4 operator/(double, const Vector4& rhs);
+ 
+Vector4 cross(const Vector4& u, const Vector4& v);
